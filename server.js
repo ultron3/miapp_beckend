182ac1b9511e380f44b708d6bd648c43cb36ca78
ultron3 <alex.gnavi@gmail.com>
2023-04-18 09:14:23 +0200

    modifica del server

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('beckend effettuato con successo');
-  }).listen(4200);
+  res.writeHead(200, { 'Content-Type': 'text/html', });
+  res.end('beckend effettuato con successo');
+}).listen(4200);
 
- 
+//aggiunto proxy per il server
+
+var HttpsProxyAgent = require('https-proxy-agent');
+var proxyConfig = [{
+  context: '/api',
+  target: 'localhost:4200',
+  secure: false
+}];
+
+function setupForCorporateProxy(proxyConfig) {
+  var proxyServer = process.env.http_proxy || process.env.HTTP_PROXY;
+  if (proxyServer) {
+    var agent = new HttpsProxyAgent(proxyServer);
+    console.log('Using corporate proxy server: ' + proxyServer);
+    proxyConfig.forEach(function (entry) {
+      entry.agent = agent;
+    });
+  }
+  return proxyConfig;
+}
